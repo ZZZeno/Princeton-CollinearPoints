cc89df80bdd36799ce3267bd87631b301866e856
Zeno <zenocbda@gmail.com>
2020-11-05 13:56:58 +0800

    Update Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Comparator;
 
 import edu.princeton.cs.algs4.StdDraw;
@@ -7,7 +6,6 @@ public class Point implements Comparable<Point> {
     // constructs the point (x, y)
     private final int x;
     private final int y;
-    public final Comparator<Point> BY_SLOP = new BySlop();
 
     public Point(int x, int y) {
         this.x = x;
@@ -30,6 +28,9 @@ public class Point implements Comparable<Point> {
     }
 
     public int compareTo(Point that) {
+        if (that == null) {
+            throw new NullPointerException();
+        }
         // compare two points by y-coordinates, breaking ties by x-coordinates
         if (this.y > that.y) return 1;
         if (this.y < that.y) return -1;
@@ -39,23 +40,29 @@ public class Point implements Comparable<Point> {
     }
 
     public double slopeTo(Point that) {
+        if (that == null) {
+            throw new NullPointerException();
+        }
         // the slope between this point and that point
         if (that.x - this.x == 0) {
-            if (that.y > this.y) return Double.POSITIVE_INFINITY;
+            if (that.y != this.y) return Double.POSITIVE_INFINITY;
             return Double.NEGATIVE_INFINITY;
         }
-        if (that.y - this.y == 0) return 0;
+        if (that.y - this.y == 0) return +0.0;
         return (double) (that.y - this.y) / (double) (that.x - this.x);
     }
 
     //    int compare(Key v, )
     private class BySlop implements Comparator<Point> {
         public int compare(Point a, Point b) {
+            if (a == null || b == null) {
+                throw new NullPointerException();
+            }
             return Double.compare(a.slopeTo(Point.this), b.slopeTo(Point.this));
         }
     }
 
-    public Comparator<Point> slopeOrder(Point a, Point b) {
+    public Comparator<Point> slopeOrder() {
         return new BySlop();
     }
 
